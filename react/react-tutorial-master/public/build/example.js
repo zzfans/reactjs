405b445efe3a92301de067fa652739505afb42af
zzfans <zhoujundk@aliyun.com>
2016-06-14 10:10:27 +0000

    add react tutorial
    tutorial

---
+++ react/react-tutorial-master/public/build/example.js
@@ -0,0 +1,106 @@
+// tutorial1.js
+var CommentBox = React.createClass({displayName: "CommentBox",
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment) {
+    // TODO: submit to the server and refresh the list
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentBox"}, 
+        React.createElement("h1", null, "Comments"), 
+        React.createElement(CommentList, {data: this.state.data}), 
+        React.createElement(CommentForm, null)
+      )
+    );
+  }
+});
+
+// tutorial2.js
+var CommentList = React.createClass({displayName: "CommentList",  
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        React.createElement(Comment, {author: comment.author}, 
+          comment.text
+        )
+      );
+    });
+    return (
+      React.createElement("div", {className: "commentList"}, 
+        commentNodes
+      )
+    );
+  }
+});
+
+var CommentForm = React.createClass({displayName: "CommentForm",
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.value.trim();
+    var text = this.refs.text.value.trim();
+    if (!author || !text) {
+      return;
+    }
+    // TODO send request to the server
+    this.refs.author.value = '';
+    this.refs.text.value = '';
+    return;
+  },
+  render: function() {
+    return (
+      React.createElement("form", {className: "commentForm", onSubmit: this.handleSubmit}, 
+        React.createElement("input", {type: "text", placeholder: "Your name", ref: "author"}), 
+        React.createElement("input", {type: "text", placeholder: "Say something...", ref: "text"}), 
+        React.createElement("input", {type: "submit", value: "Post"})
+      )
+    );
+  }
+});
+
+// tutorial3
+var Comment = React.createClass({displayName: "Comment",
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup};
+  },
+  render: function() {
+    return (
+      React.createElement("div", {className: "comment"}, 
+        React.createElement("h2", {className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.createElement("span", {dangerouslySetInnerHTML: this.rawMarkup()})
+      )
+    );
+  }
+});
+
+// tutorial4
+var data = [
+  {author: "zhoujun", text: "This is *one* comment"},
+  {author: "xiaolu", text: "This is *another* comment"}
+];
+
+ReactDOM.render(
+  React.createElement(CommentBox, {url: "api/comments.json", pollInterval: 2000}),
+  document.getElementById('content')
+);
---
+++ react/react-tutorial-master/public/scripts/example.js
@@ -0,0 +1,106 @@
+// tutorial1.js
+var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment) {
+    // TODO: submit to the server and refresh the list
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+// tutorial2.js
+var CommentList = React.createClass({  
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.value.trim();
+    var text = this.refs.text.value.trim();
+    if (!author || !text) {
+      return;
+    }
+    // TODO send request to the server
+    this.refs.author.value = '';
+    this.refs.text.value = '';
+    return;
+  },
+  render: function() {
+    return (
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+// tutorial3
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return {__html: rawMarkup};
+  },
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    );
+  }
+});
+
+// tutorial4
+var data = [
+  {author: "zhoujun", text: "This is *one* comment"},
+  {author: "xiaolu", text: "This is *another* comment"}
+];
+
+ReactDOM.render(
+  <CommentBox url="api/comments.json" pollInterval={2000} />,
+  document.getElementById('content')
+);
