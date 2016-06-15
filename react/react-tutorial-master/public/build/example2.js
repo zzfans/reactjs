75bc5ee6e40174199b26c81dc07d329c21b363a4
zzfans <zhoujundk@aliyun.com>
2016-06-15 15:13:46 +0800

    Add files via upload
    增加新的课程事例

---
+++ react/react-tutorial-master/public/build/example2.js
@@ -0,0 +1,21 @@
+var LikeButton = React.createClass({displayName: "LikeButton",
+  getInitialState: function() {
+    return {liked: false};
+  },
+  handleClick: function(event) {
+    this.setState({liked: !this.state.liked});
+  },
+  render: function() {
+    var text = this.state.liked ? 'like' : 'haven\'t liked';
+    return (
+      React.createElement("p", {onClick: this.handleClick}, 
+        "You ", text, " this. Click to toggle."
+      )
+    );
+  }
+});
+
+React.render(
+  React.createElement(LikeButton, null),
+  document.getElementById('example')
+);
